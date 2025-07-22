13d05a2e4edafede261044f4e6dbfa38debb4a3c
Neslihan Yurttaş <nbercin@gmail.com>
2025-07-22 22:51:23 +0300

    DbContexte giriş yapıldı

--- Ymyp67CvDataAccess/Contexts/Ymyp67CvProjectDbContext.cs
+++ Ymyp67CvDataAccess/Contexts/Ymyp67CvProjectDbContext.cs
@@ -13,9 +13,22 @@ namespace Ymyp67CvDataAccess.Contexts
     public sealed class Ymyp67CvProjectDbContext : DbContext
     {
         public DbSet<About> Abouts { get; set; }
+       
         public DbSet<Certificate> Certificates { get; set; }
-        public DbSet<Interest> Interests { get; set; }
         public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Education> Educations { get; set; }
+        public DbSet<Experience> Experiences { get; set; }
+        public DbSet<Interest> Interests { get; set; }
+        public DbSet<Language> Languages { get; set; }
+        public DbSet<PersonalInfo> PersonalInfos { get; set; }
+
+        public DbSet<Skill> Skills { get; set; }
+        public DbSet<SocialAccount> SocialAccounts { get; set; }
+
+
+
+
+
 
 
     }
