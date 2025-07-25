a46d66f716ae778f247fd1f6bf7bafd677f2f7cc
Neslihan Yurttaş <nbercin@gmail.com>
2025-07-25 21:02:17 +0300

    Data Access tamamlandı

---
+++ Ymyp67CvDataAccess/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.UnitOfWorks;
+using Ymyp67CvDataAccess.Contexts;
+
+namespace Ymyp67CvDataAccess.UnitOfWork
+{
+    public class UnitOfWork(Ymyp67CvProjectDbContext context) : IUnitOfWork
+    {
+        private readonly Ymyp67CvProjectDbContext _context = context;
+
+        public void Commit()
+        {
+          _context.SaveChanges();
+        }
+
+        public  async Task CommitAsync()
+        {
+           await _context.SaveChangesAsync();
+        }
+    }
+}
